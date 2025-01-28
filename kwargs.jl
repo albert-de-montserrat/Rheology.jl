fd596cf93c82763a6cc556e54da17997afd49549
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-01-28 14:59:47 +0100

    differentiable kwarg splitting

--- kwargs.jl
+++ kwargs.jl
@@ -16,117 +16,28 @@ end
     return (; zip(k, vals)...)
 end
 
+# dummy NamedTuple allocators
+@inline differentiable_kwargs(::Type{T}, ::typeof(compute_shear_strain)) where T      = (; τ = zero(T),)
+@inline differentiable_kwargs(::Type{T}, ::typeof(compute_volumetric_strain)) where T = (; τ = zero(T), P = zero(T))
+@inline differentiable_kwargs(::Type{T}, ::typeof(compute_lambda)) where T            = (; τ = zero(T), P = zero(T))
+@inline differentiable_kwargs(::Type{T}, ::typeof(compute_stress)) where T            = (; ε = zero(T),)
+@inline differentiable_kwargs(::Type{T}, ::typeof(compute_pressure)) where T          = (; θ = zero(T),)
 
-@inline differentiable_kwargs(::typeof(compute_shear_strain)) = (:τ,)
-@inline differentiable_kwargs(::typeof(compute_volumetric_strain)) = :τ, :P
-@inline differentiable_kwargs(::typeof(compute_lambda)) = :τ, :P
-@inline differentiable_kwargs(::typeof(compute_stress)) = (:ε,)
-@inline differentiable_kwargs(::typeof(compute_pressure)) = (:θ,)
+differentiable_kwargs(funs::NTuple{N, Any}) where N = differentiable_kwargs(Float64, funs)
 
-differentiable_kwargs(funs::NTuple{N, Any}) where N = flatten_repeated_functions(Base.IteratorsMD.flatten(differentiable_kwargs.(funs)))
-
-length_difference(::NTuple{N1}, ::NTuple{N2}) where {N1, N2} = Val(N1-N2)
-
-nondifferentiable_kwargs(v::NTuple{N}, k::NTuple{N}, dk::NTuple{N}, ::Val{0}) where {N} = (; zip(k, v)...)
-
-# @generated function nondifferentiable_kwargs(v::NTuple{N1, Number}, k::NTuple{N1, Symbol}, dk::NTuple{N2, Symbol}) where {N1, N2}
-#     quote
-#         @inline 
-#         # not_dk = Base.@ntuple $N1 i -> (k[i] ∉ dk ? (k[i],) : ())
-#         # Base.IteratorsMD.flatten(not_dk)
-
-#         Base.Base.@nexprs $N2 i -> begin
-#             Base.Base.@nexprs $N1 j -> begin
-#         (k[i] ∉ dk ? (k[i],) : ())
-#         Base.Base.@nexprs $N1 i -> (k[i] ∉ dk ? (k[i],) : ())
-
-#     end
-# end
-
-
-@generated function nondifferentiable_kwargs_inds(v::NTuple{N1}, k::NTuple{N1}, dk::NTuple{N2}, ::Val{ND}) where {N1, N2, ND}
-    f(k, dk, ::Val{N}) where N = MVector{N}(k[i] ∈ dk for i in 1:N)
-    f(::Val{N})        where N = MVector{N}(0 for i in 1:N)
-    
+@generated function differentiable_kwargs(::Type{T}, funs::NTuple{N, Any}) where {N, T}
     quote
         @inline 
-        flags = f(k, dk, Val($N1))
-        inds = f(Val($ND))
-        Base.@nexprs $ND i -> begin
-            Base.@nexprs $N1 j -> begin
-                if flags[j] == false && !(dk[i] === k[j])
-                    inds[i] = j
-                    flags[j] = true
-                end
-            end
-        end
-        # tuple(inds...)
-
-        kk = Base.@ntuple $ND i -> k[inds[i]]
-        vv = Base.@ntuple $ND i -> v[inds[i]]
-        # kk, vv
-        NamedTuple{kk}(vv)
-        # pk=:potato
-        # pv=1
-        # (; zip(kk, vv)...)
+        Base.@nexprs $N i -> nt_i = differentiable_kwargs($T, funs[i])
+        Base.@ncall $N merge nt
     end
 end
 
-@code_warntype nondifferentiable_kwargs_inds(v, k, dk, l_diff)
-@b nondifferentiable_kwargs_inds($(v, k, dk, l_diff)...)
-inds = nondifferentiable_kwargs_inds(v, k, dk, l_diff)
-
-function bar(k::NTuple, v::NTuple, inds::NTuple{N}) where N
-    kk = ntuple(Val(N)) do i
-        @inline
-        k[inds[i]]
-    end
-    vv = ntuple(Val(N)) do i
-        @inline
-        v[inds[i]]
-    end
-    kk,vv
-    # (; zip(kk, vv)...)
+function split_args(args, statefuns::NTuple{N, Any}) where N
+    # split args into differentiable and not differentiable
+    dummy = differentiable_kwargs(statefuns)
+    args_nondiff = Base.structdiff(args, dummy)
+    args_diff = Base.structdiff(args, args_nondiff)
+    
+    return args_diff, args_nondiff
 end
-
-@code_warntype bar(k, v, inds)
-@b bar($(k, v, inds)...)
-
-kk, vv = bar(k, v, inds)
-h(kk, vv) = (; zip(kk, vv)...)
-@code_warntype h((:a,), (2,))
-
-ntuple(Val(length(inds))) do i
-    k[inds[i]]
-end
-
-@code_warntype ntuple(Val(1)) do i
-    inds[i]
-end
-
-@b nondifferentiable_kwargs($(v, k, dk, l_diff)...)
-
-
-@code_warntype nondifferentiable_kwargs(v, k, dk)
-@code_warntype nondifferentiable_kwargs(k, dk)
-@b nondifferentiable_kwargs($(v, k, dk)...)
-@b nondifferentiable_kwargs($(k, dk)...)
-
-@b differentiable_kwargs($statefuns)
-
-dk     = differentiable_kwargs(statefuns)
-k      = keys(args)
-v      = values(args)
-non_dk = nondifferentiable_kwargs(v, k, dk)
-l_diff = length_difference(k, dk)
-
-nondifferentiable_kwargs(v, k, dk, l_diff)
-
-function foo(non_dk::NTuple{N, Symbol}, args) where N
-    ntuple(Val(N)) do i
-        @inline
-        getfield(args, non_dk[i])
-    end
-end
-
-@b foo($(non_dk, args)...)
--- matrices.jl
+++ matrices.jl
@@ -8,35 +8,24 @@
     end
 end
 
-@generated function compute_jacobian(x, composite::NTuple{N1, Any}, statefuns::NTuple{N2, Any}, args) where {N1,N2}
+@generated function compute_jacobian(x, composite::NTuple{N1, Any}, statefuns::NTuple{N2, Any}, args_diff, args_nondiff) where {N1,N2}
     quote
         J = @SMatrix zeros(N2, N2)
         Base.@nexprs $N1 i -> begin # this will be put into a function, hardcoded for now
-            J += _compute_jacobian(x, composite[i], statefuns, args)
+            J += _compute_jacobian(x, composite[i], statefuns,  args_diff, args_nondiff)
         end
         J
     end
 end
 
-function _compute_jacobian(x::SVector{N}, compositeᵢ, statefuns, args) where N
+function _compute_jacobian(x::SVector{N}, compositeᵢ, statefuns, args_diff::NamedTuple{na}, args_nondiff::NamedTuple{nb}) where {N, na, nb}
     f = x -> begin
-        k = keys(args)
-        v = values(args)
-        nondiff_vals = v[N+1:end]
-        diff_vals = tuple(x..., nondiff_vals...)
-        diff_args = (; zip(k, diff_vals)...)
-        eval_state_functions(statefuns, compositeᵢ, diff_args)    
+        k = keys(args_diff)
+        v = tuple(x...)
+        args_diff2 = (; zip(k, v)...)
+        args = merge(args_diff2, args_nondiff)
+        eval_state_functions(statefuns, compositeᵢ, args)    
     end
     return ForwardDiff.jacobian(x -> f(x), x)
 end
 
-# compute_jacobian(x, composite, statefuns, args)
-# @b compute_jacobian($(x, composite, statefuns, args)...)
-
-# # compute the global jacobian and residual
-# R = -SA[values(vars)...]
-# J = @SMatrix zeros(length(statefuns), length(statefuns))
-# Base.@nexprs 3 i -> begin # this will be put into a function, hardcoded for now
-#     J += ForwardDiff.jacobian( x-> eval_state_functions(statefuns, composite[i], (; τ = x[1], P = x[2], dt = dt)), x)
-#     R += eval_state_functions(statefuns, composite[i], args)
-# end
