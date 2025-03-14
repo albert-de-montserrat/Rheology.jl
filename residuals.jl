0cf22bb9eb64b605424f9db37343e20c1500a739
albert-de-montserrat <albert.de.montserrat@gmail.com>
2025-03-14 14:56:35 +0100

    some working Jacobians (WIP)

--- residuals.jl
+++ residuals.jl
@@ -18,7 +18,7 @@ c0, x0, vars0, args0, others0 = let
     s1     = SeriesModel(viscous1, viscous2)
     p      = ParallelModel(viscous1, viscous2)
     c      = SeriesModel(viscous1, p)
-    vars   = (; ϵ = 1e-15) # input variables (constant)
+    vars   = (; ε = 1e-15) # input variables (constant)
     args   = (; τ = 1e2) # guess variables (we solve for these, differentiable)
     others = (;)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -40,14 +40,14 @@ c1, x1, vars1, args1, others1 = let
     s1     = SeriesModel(viscous1, viscous2)
     p      = ParallelModel(s1, viscous2)
     c      = SeriesModel(viscous1, p)
-    vars   = (; ϵ = 1e-15) # input variables (constant)
+    vars   = (; ε = 1e-15) # input variables (constant)
     args   = (; τ = 1e2) # guess variables (we solve for these, differentiable)
     others = (;)       # other non-differentiable variables needed to evaluate the state functions
 
     x = SA[
         values(args)..., # global guess(es), solving for these
         values(vars)..., # local  guess(es)
-        values(vars)..., # local  guess(es)
+        values(args)..., # local  guess(es)
     ]
 
     c, x, vars, args, others
@@ -68,7 +68,7 @@ c2, x2, vars2, args2, others2 = let
     p  = ParallelModel(s1, viscous2)
     SeriesModel(viscous1, p)
     
-    vars   = (; ϵ = 1e-15) # input variables (constant)
+    vars   = (; ε = 1e-15) # input variables (constant)
     args   = (; τ = 1e2) # guess variables (we solve for these, differentiable)
     others = (;)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -93,7 +93,7 @@ c3, x3, vars3, args3, others3 = let
     s1     = SeriesModel(p1, viscous2)
     p      = ParallelModel(s1, viscous2)
     c      = SeriesModel(viscous1, p, p)
-    vars   = (; ϵ = 1e-15) # input variables (constant)
+    vars   = (; ε = 1e-15) # input variables (constant)
     args   = (; τ = 1e2) # guess variables (we solve for these, differentiable)
     others = (;)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -120,7 +120,7 @@ c4, x4, vars4, args4, others4 = let
     s1     = SeriesModel(p1, p1)
     p      = ParallelModel(s1, viscous2)
     c      = SeriesModel(viscous1, p)
-    vars   = (; ϵ = 1e-15) # input variables (constant)
+    vars   = (; ε = 1e-15) # input variables (constant)
     args   = (; τ = 1e2) # guess variables (we solve for these, differentiable)
     others = (;)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -145,7 +145,7 @@ c5, x5, vars5, args5, others5 = let
     s1     = SeriesModel(viscous1, viscous2)
     p      = ParallelModel(s1, viscous2)
     c      = SeriesModel(viscous1, powerlaw, p)
-    vars   = (; ϵ = 1e-15) # input variables (constant)
+    vars   = (; ε = 1e-15) # input variables (constant)
     args   = (; τ = 1e2) # guess variables (we solve for these, differentiable)
     others = (;)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -169,7 +169,7 @@ c6, x6, vars6, args6, others6 = let
     s1     = SeriesModel(viscous1, viscous2)
     p      = ParallelModel(s1, viscous2)
     c      = SeriesModel(viscous1, elastic, p)
-    vars   = (;  ϵ = 1e-15, θ = 1e-20) # input variables (constant)
+    vars   = (;  ε = 1e-15, θ = 1e-20) # input variables (constant)
     args   = (;  τ = 1e2  , P = 1e6) # guess variables (we solve for these, differentiable)
     others = (; dt = 1e9)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -193,7 +193,7 @@ c7, x7, vars7, args7, others7 = let
     s1     = SeriesModel(viscous1, viscous2)
     p      = ParallelModel(s1, viscous2)
     c      = SeriesModel(viscous1, drucker, p)
-    vars   = (; ϵ = 1e-15) # input variables (constant)
+    vars   = (; ε = 1e-15) # input variables (constant)
     args   = (; τ = 1e2) # guess variables (we solve for these, differentiable)
     others = (;)       # other non-differentiable variables needed to evaluate the state functions
 
@@ -206,13 +206,35 @@ c7, x7, vars7, args7, others7 = let
     c, x, vars, args, others
 end
 
+
+@inline differentiable_kwargs(eqs::NTuple{N, GlobalSeriesEquation}) where N = merge(differentiable_kwargs(eqs[1]), differentiable_kwargs(Base.tail(eqs))...)
+@inline differentiable_kwargs(eqs::NTuple{N, LocalParallelEquation}) where {N} = differentiable_kwargs(eqs[1]), differentiable_kwargs(Base.tail(eqs))...
+@inline differentiable_kwargs(eqs::T) where {T<:Union{GlobalSeriesEquation,LocalParallelEquation}} = differentiable_kwargs(eqs.fn)
+@inline differentiable_kwargs(::Tuple{})= ()
+
+@inline serialize(::Tuple{}) = ()
+
+function serialize(c::AbstractCompositeModel)
+    (; leafs, branches) = c
+    x = ntuple(Val(length(branches))) do i
+        @inline 
+        serialize(branches[i])
+    end
+    leafs, x...
+end
+
 function eval_series(c::SeriesModel, x::SVector, vars::NamedTuple, args::NamedTuple, others::NamedTuple)
     eqs         = SeriesModelEquations(c)
     leafs       = c.leafs
     nseries     = length(leafs)
     eqs_series  = eqs.fns_series
     neqs        = length(eqs_series)
-    args_merged = merge(args, others)
+
+    args_template = differentiable_kwargs(eqs_series)
+    keys_series   = keys(args_template)
+    val_series    = ntuple(i -> x[i], Val(nseries))
+    args_tmp      = (; zip(keys_series, val_series)...)
+    args_merged   = merge(args_tmp, vars, others)
 
     v = ntuple(Val(neqs)) do i
         @inline
@@ -236,16 +258,95 @@ function eval_series(c::SeriesModel, x::SVector, vars::NamedTuple, args::NamedTu
     end
 end
 
+function eval_parallel(c::SeriesModel, x::SVector, vars::NamedTuple, args::NamedTuple, others::NamedTuple)
+    eqs          = SeriesModelEquations(c)
+    (; branches) = c
+    nbranches    = length(branches)
+    eqs_parallel = eqs.fns_parallel
+    nseries      = length(eqs.fns_series)
+    neqs         = length(eqs_parallel)
+    
+    args_template = differentiable_kwargs(eqs_parallel)
+    keys_parallel = keys.(args_template)
+    val_parallel  = ntuple(i -> x[i + nseries], Val(neqs))
+    args_merged   = ntuple(Val( length(val_parallel) )) do i 
+        v = (; zip(keys_parallel[i], val_parallel[i])...)
+        merge(v, others)
+    end
+    
+    c_serial     = ntuple(i -> serialize(c.branches[i]), Val(nbranches)) # |> flatten
+    np           = length(c_serial)
+    
+    counter = Ref(0)
+    ntuple(Val(np)) do k
+        cₖ = c_serial[k]
+        ntuple(Val(length(cₖ))) do i
+            @inline
+            I  = counter[] += 1
+            eq = eqs_parallel[I]
+            fn = eq.fn
+            cᵢ = cₖ[i] |> superflatten
+
+            # eval functions
+            v_parallel = ntuple(Val(length(cᵢ))) do j
+                @inline
+                fn(cᵢ[j], args_merged[i])
+            end |> sum
+    
+            # # reduce values coming from local/parallel equations
+            # inds = eq.eqnums_reduce
+            # v_local = ntuple(Val(length(inds))) do j
+            #     @inline
+            #     x[inds[j]]
+            # end |> sum
+
+            # i dont like this, need to find a better way to do this
+            v_local = i < length(cₖ) ? x[eq.eqnum + 1 + nseries] : 0.0
+
+            v_parallel - x[eq.eqnum] + v_local
+        end
+    end |> flatten
+end
+
+function eval_residual(c::SeriesModel, x::SVector, vars::NamedTuple, args::NamedTuple, others::NamedTuple)
+    # args_s      = (; τ = x[1]) 
+    # vars_p      = (; ε = x[2]) 
+    r_series    =   eval_series(c, x, vars, args, others)
+    r_parallel  = eval_parallel(c, x, vars, args, others)
+    SA[r_series..., r_parallel...]
+end
+
+J0 = ForwardDiff.jacobian( x-> eval_residual(c0, x, vars0, args0, others0), x0)
+J1 = ForwardDiff.jacobian( x-> eval_residual(c1, x, vars1, args1, others),  x1)
+
+@test J0 == SA[
+    1.0e-20  1.0
+   -1.0      3.0e20
+]
+@test J1 == SA[
+    1.0e-20   1.0     0.0
+   -1.0       2.0e20  1.0
+    0.0      -1.0     1.5e-20
+]
+
+c, x, vars, args, others = c1, x1, vars1, args1, others1
+
+eval_parallel(c0, x0, vars0, args0, others0)
+eval_parallel(c1, x1, vars1, args1, others1)
+eval_parallel(c2, x2, vars2, args2, others2)
+eval_parallel(c3, x3, vars3, args3, others3)
+eval_parallel(c4, x4, vars4, args4, others4) # fails
+eval_parallel(c5, x5, vars5, args5, others5) # fails
+eval_parallel(c6, x6, vars6, args6, others6) # fails
+eval_parallel(c7, x7, vars7, args7, others7) # fails
 
 eval_series(c0, x0, vars0, args0, others0)
 eval_series(c1, x1, vars1, args1, others1)
 eval_series(c2, x2, vars2, args2, others2)
 eval_series(c3, x3, vars3, args3, others3)
-eval_series(c4, x4, vars4, args4, others4)
-eval_series(c5, x5, vars5, args5, others5)
-eval_series(c6, x6, vars6, args6, others6)
-eval_series(c7, x7, vars7, args7, others7)
-
+eval_series(c4, x4, vars4, args4, others4) # fails
+eval_series(c5, x5, vars5, args5, others5) # fails
+eval_series(c6, x6, vars6, args6, others6) # fails
+eval_series(c7, x7, vars7, args7, others7) # fails
 
 # @b eval_series($(c, x, vars, args, others)...)
-
